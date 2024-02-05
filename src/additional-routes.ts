48a616978de14bc68b73d74617be0c64d2141776
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-02-05 08:07:12 -0800

    Add attl routes

--- src/additional-routes.ts
+++ src/additional-routes.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import { AppContext } from './config'
 import redis, { RedisKeys } from './util/redis'
+import { getPodcastEmbedFeed } from './util/bsky'
 
 const makeRouter = (ctx: AppContext) => {
 	const router = express.Router()
@@ -18,6 +19,62 @@ const makeRouter = (ctx: AppContext) => {
 
 	router.get('/health', (_req, res) => {
 		res.sendStatus(200)
+	})
+
+	router.get('/db/posts', async (_req, res) => {
+		await ctx.db
+			.selectFrom('post')
+			.selectAll()
+			.orderBy('indexedAt', 'desc')
+			.execute()
+			.then(posts => {
+				res.json(posts)
+			})
+	})
+
+	router.get('/db/state', async (_req, res) => {
+		await ctx.db
+			.selectFrom('sub_state')
+			.selectAll()
+			.execute()
+			.then(state => {
+				res.json(state)
+			})
+	})
+
+	// router.get('/db/stats', async (_req, res) => {
+	// 	await ctx.db
+	// 		.selectFrom('pragma_page_count()')
+	// 		.selectAll()
+	// 		.execute()
+	// 		.then(state => {
+	// 			res.json(state)
+	// 		})
+	// })
+
+	router.get('/refresh', async (_req, res) => {
+		const feed = await getPodcastEmbedFeed()
+		console.log('feed', feed)
+		await ctx.db
+			.insertInto('post')
+			.values(
+				feed.map(({ post }) => {
+					return {
+						uri: post.uri,
+						cid: post.cid,
+						indexedAt: post.indexedAt,
+						// @ts-ignore
+						replyParent: post?.record?.reply?.parent?.uri,
+						// @ts-ignore
+						replyRoot: post?.record?.reply?.root?.uri,
+					}
+				})
+			)
+			.onConflict(oc => oc.doNothing())
+			.execute()
+			.then(state => {
+				res.json({ state })
+			})
 	})
 
 	return router
